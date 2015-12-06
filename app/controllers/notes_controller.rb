cf8b4b2c421a8437c5f30ee0a8f37826b30ffce4
Matt Fair <matt.j.fair@gmail.com>
2015-12-06 00:22:37 -0500

    changed so user couldn't enter own ID

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,27 +18,28 @@ class NotesController < ApplicationController
     if params[:note][:tags]
       params[:note][:tag_names] = params[:note].delete :tags
     end
-    if params[:note][:api_token]
-      if find_by_single_access_token(params[:note][:api_token])
-        params[:note][:user_id] = find_by_single_access_token(params[:note][:api_token]).id
-        save_note
-      else
-        respond_to do |format|
-          format.json{
-            render json: "The User information you provided did not match any user"
-          }
-        end
-      end
-    else
+    @note = Note.new(note_params)
+    if params[:note][:api_token] && find_by_single_access_token(params[:note][:api_token])
+      @note.user = find_by_single_access_token(params[:note][:api_token])
       save_note
+    elsif !params[:note][:api_token]
+      save_note
+    else
+      respond_to do |format|
+        format.json{
+          render json: {
+            errors: "The token you entered did not match any of our records.",
+            status: 400
+          }, :status=> 400
+        }
+      end
     end
-
   end
 
   private
 
   def note_params
-    params.require(:note).permit(:title, :body, :tag_names, :user_id)
+    params.require(:note).permit(:title, :body, :tag_names)
   end
 
   def single_access_allowed?
@@ -50,7 +51,7 @@ class NotesController < ApplicationController
   end
 
   def save_note
-    @note = Note.new(note_params)
+
     if @note.save
       if current_user_session
         @note.update(user_id: current_user.id)
