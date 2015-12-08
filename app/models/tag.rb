8b8df232fb254a4ae1951cf2aeabcf51efec1077
Matt Fair <matt.j.fair@gmail.com>
2015-12-07 19:38:34 -0500

    added tests for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,9 +6,14 @@ class Tag < ActiveRecord::Base
   before_validation :downcase_name
                     :strip_spaces
                     :remove_symbols
+                    :remove_spaces
 
   def remove_symbols
     self.name = self.name.gsub(/[^0-9a-z ]/i, '')
+  end
+
+  def remove_spaces
+    self.name = self.name.gsub(' ', '_')
   end
 
   def downcase_name
@@ -22,8 +27,9 @@ class Tag < ActiveRecord::Base
   def self.clean_name(name)
     tag = Tag.new(name: name)
     tag.strip_spaces
-    tag.downcase_name
     tag.remove_symbols
+    tag.downcase_name
+    tag.remove_spaces
   end
 
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
+  include FactoryGirl::Syntax::Methods
   # test "the truth" do
   #   assert true
   # end
+
+  test "downcase_tag" do
+    note = create(:note, :tag_names =>"TAG, TeSt")
+    assert note.tags.first.name == "tag", message:"all caps didnt work"
+    assert note.tags[1].name == "test", message:"some caps didnt work"
+  end
+
+  test "strip_tag" do
+    note = create(:note, :tag_names =>"    tag   , test  ")
+    assert note.tags.first.name == "tag", message:"strip didnt work"
+    assert note.tags[1].name == "test", message:"strip 2 didnt work"
+  end
+
+  test "no_spaces" do
+    note = create(:note, :tag_names =>"    t a g , t e s t  ")
+    puts note.tags.first.name
+    assert note.tags.first.name == "t_a_g", message:"no spaces didnt work"
+    assert note.tags[1].name == "t_e_s_t", message:"no spaces 2 didnt work"
+  end
+
+  test "no_symbolss" do
+    note = create(:note, :tag_names =>"    t#a*g , t$e%s&t  ")
+    puts note.tags.first.name
+    assert note.tags.first.name == "tag", message:"no spaces didnt work"
+    assert note.tags[1].name == "test", message:"no spaces 2 didnt work"
+  end
+
 end
